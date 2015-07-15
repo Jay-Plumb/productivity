62ec584da0fbb0d4b17eb9b1806238eca2b56b11
Jay-Plumb <jaymjplumb@gmail.com>
2015-07-15 15:17:28 -0700

    resolving scss automatic compile

--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -1,7 +1,7 @@
 module.exports = function(grunt) {
 
 grunt.initConfig({
-	clean: {
+  clean: {
   build: {
     src: ["scss/app.scss"]
   }
@@ -15,28 +15,28 @@ grunt.initConfig({
            dest: 'scss/app.scss',
          }
        },
+  sass: {
+    dist: {
+      files: {
+        'css/styles.css' : 'scss/app.scss'
+      }
+    }
+  },
+  watch: {
+    css: {
+      files: 'scss/**/*.scss',
+      tasks: ['clean','concat','sass']
+    },
+    bundle: {
+     files: 'app.js',
+     tasks: ['browserify']
+   }
 
+  },
 
-	sass: {
-				dist: {
-					files: {
-						'css/styles.css' : 'scss/app.scss'
-					}
-				}
-			},
-			watch: {
-				css: {
-					files: 'scss/**/*.scss',
-					tasks: ['clean','concat','sass'],
-					options: {
-						livereload: true
-					}
-				}
-			},
-
-		// browserify
-	browserify: {
-  	js: {
+    // browserify
+  browserify: {
+    js: {
       src: ['app.js'], //ideally src is templates/**/*.hbs rather than on a specific module
       dest: 'bundle.js'
     },
@@ -45,14 +45,14 @@ grunt.initConfig({
       // watch: true,
       // keepAlive: true
     }
-	}
+  }
 
 });
 
-	grunt.loadNpmTasks('grunt-browserify');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.registerTask('default', ['browserify', 'watch'])
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.registerTask('default', ['browserify', 'watch'])
 };
