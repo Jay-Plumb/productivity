3b5c64f13164951c15fdd354409162bdfd2379d8
Jay-Plumb <jaymjplumb@gmail.com>
2015-07-15 14:30:55 -0700

    issue with compiling multiple scss files. Working now

--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -1,22 +1,58 @@
 module.exports = function(grunt) {
 
-	grunt.initConfig({
+grunt.initConfig({
+	clean: {
+  build: {
+    src: ["scss/app.scss"]
+  }
+},
+
+  concat: {
+         dist: {
+           src: [
+             'scss/*.scss',
+           ],
+           dest: 'scss/app.scss',
+         }
+       },
+
+
+	sass: {
+				dist: {
+					files: {
+						'css/styles.css' : 'scss/app.scss'
+					}
+				}
+			},
+			watch: {
+				css: {
+					files: 'scss/**/*.scss',
+					tasks: ['clean','concat','sass'],
+					options: {
+						livereload: true
+					}
+				}
+			},
 
 		// browserify
-		browserify: {
-    js: {
-        src: ['app.js'], //ideally src is templates/**/*.hbs rather than on a specific module
-        dest: 'bundle.js'
+	browserify: {
+  	js: {
+      src: ['app.js'], //ideally src is templates/**/*.hbs rather than on a specific module
+      dest: 'bundle.js'
     },
     options: {
-        transform: ['hbsfy'],
-        watch: true,
-        keepAlive: true
+      transform: ['hbsfy'],
+      // watch: true,
+      // keepAlive: true
     }
-}
+	}
 
-	});
+});
 
 	grunt.loadNpmTasks('grunt-browserify');
-	grunt.registerTask('default', ['browserify'])
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.registerTask('default', ['browserify', 'watch'])
 };
--- grunt/app.js
+++ grunt/app.js
@@ -1,8 +1,9 @@
+// This will be the start of the app
 var $ = require('jquery');
 var template = require("./templates/helloWorld.hbs");
 var template2 = require("./templates/byeWorld.hbs");
 //document.body.innerHTML = template({ name: "jssn" });
- $("body").append(template({ name: "jssn" }));
- $("body").append(template2({ name: "jssn" }));
+ $("body").append(template({ name: "jason" }));
+ $("body").append(template2({ name: "jay" }));
 
 $('body').css('background','gray');
